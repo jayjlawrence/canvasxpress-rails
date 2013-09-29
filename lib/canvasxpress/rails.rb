2a54fb56eafaceb0f2d94530d9b2646df43f8c51
Jay Lawrence <jay@patientway.com>
2013-09-29 11:22:29 -0400

    Draft packaging

---
+++ lib/canvasxpress/rails.rb
@@ -0,0 +1,7 @@
+require "canvasxpress/rails/version"
+
+module Canvasxpress
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/canvasxpress/rails/version.rb
@@ -0,0 +1,5 @@
+module Canvasxpress
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
